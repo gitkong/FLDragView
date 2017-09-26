dc773afbf121b2bb3b217b09c6626d5be7863271
gitKong <13751855378@163.com>
2017-09-26 11:41:02 +0800

    update

--- FLDragViewDemo/FLSlideView/AppDelegate.m
+++ FLDragViewDemo/FLSlideView/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] init];
+    ViewController *vc = [[ViewController alloc] init];
+    vc.view.backgroundColor = [UIColor whiteColor];
+    self.window.rootViewController = vc;
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
--- FLDragViewDemo/FLSlideView/ViewController.m
+++ FLDragViewDemo/FLSlideView/ViewController.m
@@ -9,9 +9,12 @@
 #import "ViewController.h"
 #import "UIView+drag.h"
 #import "FLView.h"
+#import <AVFoundation/AVFoundation.h>
 @interface ViewController ()
 
 @property (nonatomic,weak)UIView *firstView;
+@property (nonatomic, strong) AVPlayer *player;
+@property (nonatomic, strong) AVPlayerLayer * playerLayer;
 @end
 
 @implementation ViewController
@@ -40,13 +43,26 @@
     view1.backgroundColor = [UIColor lightGrayColor];
     [self.view addSubview:view1];
     
-    UIView *view2 = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
-    view2.backgroundColor = [UIColor redColor];
+    UIView *view2 = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
+//    view2.backgroundColor = [UIColor redColor];
+    
     
     view2.fl_canSlide = YES;
     view2.fl_bounces = NO;
     view2.fl_isAdsorb = YES;
     [view1 addSubview:view2];
+    
+    
+    self.player = [AVPlayer playerWithURL:[NSURL URLWithString:@"http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"]];
+    [self.player play];
+    self.player.usesExternalPlaybackWhileExternalScreenIsActive=YES;
+    self.playerLayer=[[AVPlayerLayer alloc]init];
+    self.playerLayer.backgroundColor=[UIColor blackColor].CGColor;
+    self.playerLayer.player=self.player;
+    self.playerLayer.frame=view2.bounds;
+    [self.playerLayer displayIfNeeded];
+    [view2.layer insertSublayer:self.playerLayer atIndex:0];
+    self.playerLayer.videoGravity=AVLayerVideoGravityResizeAspect;
     
     FLView *view3 = [[FLView alloc] initWithFrame:CGRectMake(250, 0, 50, 50)];
     view3.backgroundColor = [UIColor greenColor];
